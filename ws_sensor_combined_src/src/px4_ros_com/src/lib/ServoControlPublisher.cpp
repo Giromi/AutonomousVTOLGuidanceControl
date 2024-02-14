63ab1d11f8175036fd23b77c8b83c3354e83b2b2
giromi <giromi@cau.ac.kr>
2024-02-14 13:19:47 +0900

    2024-02-14 시작

--- ws_sensor_combined_src/src/px4_ros_com/src/lib/ServoControlPublisher.cpp
+++ ws_sensor_combined_src/src/px4_ros_com/src/lib/ServoControlPublisher.cpp
@@ -13,9 +13,16 @@ void ServoControlPublisher::_publish_servo_control_message(void)
 {
     auto message = mavros_msgs::msg::ActuatorControl();
     message.header.stamp = this->get_clock()->now();
-    message.group_mix = 2;  // Use group 2 for AUX channels in PX4
+    message.group_mix = 3;  // Use group 2 for AUX channels in PX4
     message.controls[0] = 0.5;  // Example: set midpoint (1500 μs in PWM) for the first AUX channel
     message.controls[1] = 0.5;  // Example: set midpoint (1500 μs in PWM) for the first AUX channel
+    message.controls[2] = 0.5;  // Example: set midpoint (1500 μs in PWM) for the first AUX channel
+    message.controls[3] = 0.5;  // Example: set midpoint (1500 μs in PWM) for the first AUX channel
+    message.controls[4] = 0.5;  // Example: set midpoint (1500 μs in PWM) for the first AUX channel
+    message.controls[5] = 0.5;  // Example: set midpoint (1500 μs in PWM) for the first AUX channel
+    message.controls[6] = 0.5;  // Example: set midpoint (1500 μs in PWM) for the first AUX channel
+    message.controls[7] = 0.5;  // Example: set midpoint (1500 μs in PWM) for the first AUX channel
+
 
     publisher_->publish(message);
 }
