e692a56df5d7253fc6ade33310b31b437a455c6f
giromi <giromi@cau.ac.kr>
2024-02-20 15:18:41 +0900

    what

---
+++ .backup/install/px4_ros2_cpp/include/px4_ros2/odometry/local_position.hpp
@@ -0,0 +1,66 @@
+/****************************************************************************
+ * Copyright (c) 2023 PX4 Development Team.
+ * SPDX-License-Identifier: BSD-3-Clause
+ ****************************************************************************/
+
+#pragma once
+
+#include <Eigen/Eigen>
+#include <px4_msgs/msg/vehicle_local_position.hpp>
+#include <px4_ros2/common/context.hpp>
+#include <px4_ros2/odometry/subscription.hpp>
+
+namespace px4_ros2
+{
+/** \ingroup odometry
+ *  @{
+ */
+
+/**
+ * Provides access to the vehicle's local position estimate
+ */
+class OdometryLocalPosition : public Subscription<px4_msgs::msg::VehicleLocalPosition>
+{
+public:
+  explicit OdometryLocalPosition(Context & context);
+
+  bool positionXYValid() const
+  {
+    return lastValid() && last().xy_valid;
+  }
+
+  bool positionZValid() const
+  {
+    return lastValid() && last().z_valid;
+  }
+
+  Eigen::Vector3f positionNed() const
+  {
+    const px4_msgs::msg::VehicleLocalPosition & pos = last();
+    return {pos.x, pos.y, pos.z};
+  }
+
+  bool velocityXYValid() const
+  {
+    return lastValid() && last().v_xy_valid;
+  }
+
+  bool velocityZValid() const
+  {
+    return lastValid() && last().v_z_valid;
+  }
+  Eigen::Vector3f velocityNed() const
+  {
+    const px4_msgs::msg::VehicleLocalPosition & pos = last();
+    return {pos.vx, pos.vy, pos.vz};
+  }
+
+  Eigen::Vector3f accelerationNed() const
+  {
+    const px4_msgs::msg::VehicleLocalPosition & pos = last();
+    return {pos.ax, pos.ay, pos.az};
+  }
+};
+
+/** @}*/
+} /* namespace px4_ros2 */
---
+++ .backup/install/px4_ros_com/include/px4_ros_com/ServoControlPublisher.hpp
@@ -0,0 +1,14 @@
+#include <rclcpp/rclcpp.hpp>
+#include <mavros_msgs/msg/actuator_control.hpp>
+
+class ServoControlPublisher : public rclcpp::Node {
+public:
+    ServoControlPublisher(void);
+
+private:
+    void _publish_servo_control_message(void);
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<mavros_msgs::msg::ActuatorControl>::SharedPtr publisher_;
+};
+
