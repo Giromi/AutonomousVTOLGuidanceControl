6d25ea74c386e550d2b46f336c2a7cfb9f29aba2
haechan <haechan@haechan.(none)>
2024-07-02 19:00:00 +0900

    key event에 사용 되는 static 함수는 _를 앞에 붙인 camelCase로 변경하였고 ( snake_case -> _camelCase) 일반 함수도 camelCase로 변경했습니다. ( snake_case -> camelCase) static  변수가 아닌데 _snake_case인 변수를 snake_case로 변경했습니다.(_snake_case -> snake_case)

--- ws_sensor_combined_src/src/px4_ros_com/src/main/key_event_pub.cpp
+++ ws_sensor_combined_src/src/px4_ros_com/src/main/key_event_pub.cpp
@@ -1,7 +1,7 @@
 #include "key_event/KeyPublisher.hpp"
 
 int main(int argc, char **argv) {
-	signal(SIGINT, KeyPublisher::sigint_handler);
+	signal(SIGINT, KeyPublisher::sigintHandler);
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<KeyPublisher>());
     rclcpp::shutdown();
