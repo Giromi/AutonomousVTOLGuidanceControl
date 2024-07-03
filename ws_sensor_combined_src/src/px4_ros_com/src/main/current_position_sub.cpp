8a82f83e4b875528bc5a4d1a58ab3706013fad1e
haechan <haechan@haechan.(none)>
2024-07-03 15:35:54 +0900

    feat:  current_position_sub.cpp position_sub.cpp convention 업데이트 했습니다.

--- ws_sensor_combined_src/src/px4_ros_com/src/main/current_position_sub.cpp
+++ ws_sensor_combined_src/src/px4_ros_com/src/main/current_position_sub.cpp
@@ -5,25 +5,25 @@ class CurrentPostionSubscriber : public rclcpp::Node {
 public: 
     CurrentPostionSubscriber() : Node("offboard_mavros_local_position") {
        auto default_qos = rclcpp::QoS(rclcpp::SystemDefaultsQoS());
-        current_pos_sub_ = create_subscription<geometry_msgs::msg::PoseStamped>("/mavros/local_position/pose", default_qos,
-        std::bind(&CurrentPostionSubscriber::currentpositionCallback, this, std::placeholders::_1
+        current_pos_sub = create_subscription<geometry_msgs::msg::PoseStamped>("/mavros/local_position/pose", default_qos,
+        std::bind(&CurrentPostionSubscriber::currentPositionCallback, this, std::placeholders::_1
         ));
     }
 private:
-    void currentpositionCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg) {
-        current_position_ = {msg->pose.position.x, msg->pose.position.y, msg->pose.position.z};
+    void currentPositionCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg) {
+        _current_position = {msg->pose.position.x, msg->pose.position.y, msg->pose.position.z};
         std::cout.precision(2);
         std::cout << "현재 위치" << std::endl;
-        std::cout << "EAST : " << current_position_[0] << "\n"
-        << "NORTH : " << current_position_[1] << "\n"
-        << "UP : " << current_position_[2] << std::endl;
+        std::cout << "EAST : " << _current_position[0] << "\n"
+        << "NORTH : " << _current_position[1] << "\n"
+        << "UP : " << _current_position[2] << std::endl;
     }
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr    current_pos_sub_;
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr    current_pos_sub;
 
-    static std::array<double, 3>		    current_position_;
+    static std::array<double, 3>		    _current_position;
 };
 
-std::array<double, 3>		            CurrentPostionSubscriber::current_position_{};
+std::array<double, 3>		            CurrentPostionSubscriber::_current_position{};
 
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
