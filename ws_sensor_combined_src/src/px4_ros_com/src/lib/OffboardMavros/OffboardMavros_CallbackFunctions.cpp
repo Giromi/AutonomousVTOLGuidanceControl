1550475e61586ea83267e8b63b45666214d42133
giromi <giromi@cau.ac.kr>
2024-07-12 00:03:07 +0900

    feat: conflict 해결

--- ws_sensor_combined_src/src/px4_ros_com/src/lib/OffboardMavros/OffboardMavros_CallbackFunctions.cpp
+++ ws_sensor_combined_src/src/px4_ros_com/src/lib/OffboardMavros/OffboardMavros_CallbackFunctions.cpp
@@ -385,6 +385,7 @@ void OffboardMavros::locationResponseCallback(const rclcpp::Client<mavros_msgs::
     printSuccessInfo(future.get()->success, msg);
 }
 
+<<<<<<< HEAD
 void OffboardMavros::currentPositionCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg) {
     _cur_position = {msg->pose.position.x, msg->pose.position.y, msg->pose.position.z};
     if (_cmd_flag == vtol::TAKEOFF) {
@@ -394,6 +395,18 @@ void OffboardMavros::currentPositionCallback(const geometry_msgs::msg::PoseStamp
             _prev_position[vtol::EAST] = _cur_position[vtol::EAST];
             DEBUG::print("Landing point North :", _prev_position[vtol::NORTH], BOLDYELLOW);
             DEBUG::print("Landing point East  :", _prev_position[vtol::EAST], BOLDYELLOW);
+=======
+void OffboardMavros::currentpositionCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg) {
+    cur_position_ = {msg->pose.position.x, msg->pose.position.y, msg->pose.position.z};
+
+    if (cmdFlag_ == vtol::TAKEOFF) {
+        if (global_position_[vtol::ALT] > init_global_position[vtol::ALT] - 1) {
+            cmdFlag_ = vtol::FLY;
+            prev_position_[vtol::NORTH] = cur_position_[vtol::NORTH];
+            prev_position_[vtol::EAST] = cur_position_[vtol::EAST];
+            DEBUG::print("Landing point North :", prev_position_[vtol::NORTH], BOLDYELLOW);
+            DEBUG::print("Landing point East  :", prev_position_[vtol::EAST], BOLDYELLOW);
+>>>>>>> 62e6b807 (feat:  절대 좌표계로 일정 고도만큼 상승)
         }
     } else if (_cmd_flag == vtol::TO_FIXED) {
         DEBUG::print("North :", _cur_position[vtol::NORTH], WHITE);
