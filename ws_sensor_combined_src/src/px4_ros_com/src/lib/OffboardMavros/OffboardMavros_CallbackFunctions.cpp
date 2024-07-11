5085e54bfcdcede6d9e411ce493ba724d51314fb
giromi <giromi@cau.ac.kr>
2024-07-11 20:15:54 +0900

    feat: position

--- ws_sensor_combined_src/src/px4_ros_com/src/lib/OffboardMavros/OffboardMavros_CallbackFunctions.cpp
+++ ws_sensor_combined_src/src/px4_ros_com/src/lib/OffboardMavros/OffboardMavros_CallbackFunctions.cpp
@@ -138,6 +138,13 @@ void OffboardMavros::stateCommandFixed (void) {
     // }
 }
 
+void OffboardMavros::stateCommandFixed (void) {
+    RCLCPP_INFO(this->get_logger(), "< State Command To Fixed >");
+    if (fcu_state.mode != vtol::FCU_POSITION) {
+        updatePositionMode();
+    }
+}
+
 void OffboardMavros::stateCommandStart (void) { 
     RCLCPP_INFO(this->get_logger(), "< State Command Start >");
     if (fcu_state.mode == vtol::FCU_HOLD) {
