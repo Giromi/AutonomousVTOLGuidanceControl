61e9600182cff1b1e3f2bc4b79981cf5d472ad7f
giromi <giromi@cau.ac.kr>
2024-07-11 16:44:48 +0900

    feat: action 수정 및 origin 출력 제거

--- ws_sensor_combined_src/src/px4_ros_com/src/lib/OffboardMavros/OffboardMavros_Action.cpp
+++ ws_sensor_combined_src/src/px4_ros_com/src/lib/OffboardMavros/OffboardMavros_Action.cpp
@@ -114,12 +114,10 @@ void OffboardMavros::_actionTakeoff(void) {
     // if (!(statusFlag & vtol::BIT_FLY)) {
     //     RCLCPP_INFO(this->get_logger(), "Vehicle is NOT ARMED status");
     //     return true;
-    if (OffboardMavros::_cmd_flag == vtol::READY) {
-        std::cout << "Vehicle is NOT ARMED status" << std::endl;
-        return ;
-    } else if (OffboardMavros::_cmd_flag == vtol::ARMED) {
-        std::cout << "Calling takeoff service ..." << std::endl;
-    }
+    // if (OffboardMavros::_cmd_flag == vtol::READY) {
+    //     std::cout << "Vehicle is NOT READY status" << std::endl;
+    //     return ;
+    // }
     OffboardMavros::_cmd_flag = vtol::TAKEOFF;
 }
 
