f0e35eb732eb184479fb9eb1d94c43f2bdd163ed
haechan <haechan@haechan.(none)>
2024-07-15 14:30:28 +0900

    feat: 모든 pub 내부 변수 msg 통일 및 스마트포인터 사용 완료 했습니다.

--- ws_sensor_combined_src/src/px4_ros_com/src/lib/OffboardMavros/OffboardMavros_PublishFunctions.cpp
+++ ws_sensor_combined_src/src/px4_ros_com/src/lib/OffboardMavros/OffboardMavros_PublishFunctions.cpp
@@ -30,129 +30,122 @@ void    OffboardMavros::publish(void) {
 
 void OffboardMavros::publishCmdVel(void) {
     RCLCPP_INFO(this->get_logger(), "publishing cmd_vel");
-    geometry_msgs::msg::TwistStamped cmd_vel;
-    cmd_vel.header.stamp = this->now();
-    cmd_vel.header.frame_id = "standard_vtol_0";
-    cmd_vel.twist.linear.x = _local_velocity[0];
-    cmd_vel.twist.linear.y = _local_velocity[1];
-    cmd_vel.twist.linear.z = _local_velocity[2];
-    cmd_vel.twist.angular.x = _local_velocity[3];
-    cmd_vel.twist.angular.y = _local_velocity[4];
-    cmd_vel.twist.angular.z = _local_velocity[5];
-    att_pub->publish(cmd_vel);
+    auto msg = std::make_shared<geometry_msgs::msg::TwistStamped>();
+    msg->header.stamp = this->now();
+    msg->header.frame_id = "standard_vtol_0";
+    msg->twist.linear.x = _local_velocity[0];
+    msg->twist.linear.y = _local_velocity[1];
+    msg->twist.linear.z = _local_velocity[2];
+    msg->twist.angular.x = _local_velocity[3];
+    msg->twist.angular.y = _local_velocity[4];
+    msg->twist.angular.z = _local_velocity[5];
+    att_pub->publish(*msg);
 }
 
 void OffboardMavros::publishManual(void) {
     RCLCPP_INFO(this->get_logger(), "publishing manual");
-    mavros_msgs::msg::ManualControl manual;
-    manual.header.stamp = this->now();
-    manual.header.frame_id = "standard_vtol_0";
-    manual.x = _local_velocity[0];
-    manual.y = _local_velocity[1];
-    manual.z = _local_velocity[2];
-    manual.r = _local_velocity[3];
-    manual.buttons = 10;
-    vc_manual_pub->publish(manual);
-    
-
+    auto msg = std::make_shared<mavros_msgs::msg::ManualControl>();
+    msg->header.stamp = this->now();
+    msg->header.frame_id = "standard_vtol_0";
+    msg->x = _local_velocity[0];
+    msg->y = _local_velocity[1];
+    msg->z = _local_velocity[2];
+    msg->r = _local_velocity[3];
+    msg->buttons = 10;
+    vc_manual_pub->publish(*msg);
 }
 
 void OffboardMavros::publishGpOrigin(void) {
     // RCLCPP_INFO(this->get_logger(), "publishing origin");
-    geographic_msgs::msg::GeoPoseStamped origin;
-    origin.header.stamp = this->now();
-    origin.header.frame_id = "standard_vtol_0";
-    origin.pose.position.altitude = 47.6554;
-    origin.pose.position.latitude = 47.3984;
-    origin.pose.position.longitude = 8.54616;
-
-    gp_origin_pub->publish(origin);
+    auto msg = std::make_shared<geographic_msgs::msg::GeoPoseStamped>();
+    msg->header.stamp = this->now();
+    msg->header.frame_id = "standard_vtol_0";
+    msg->pose.position.altitude = 47.6554;
+    msg->pose.position.latitude = 47.3984;
+    msg->pose.position.longitude = 8.54616;
+    gp_origin_pub->publish(*msg);
     // DEBUG::print("publishing origin",     mavros_msgs::msg::State::MODE_PX4_MANUAL, BOLDGREEN);
-;
 }
 
 void OffboardMavros::publishPose(void) {
-    geometry_msgs::msg::PoseStamped pose;
-    pose.pose.position.x = _local_position[vtol::EAST];
-    pose.pose.position.y = _local_position[vtol::NORTH];
-    pose.pose.position.z = _local_position[vtol::UP];
-    pose.pose.orientation.x = _local_velocity[3];
-    pose.pose.orientation.y = _local_velocity[4];
-    pose.pose.orientation.z = _local_velocity[5];
-    local_pos_pub->publish(pose);
+    auto msg = std::make_shared<geometry_msgs::msg::PoseStamped>();
+    msg->pose.position.x = _local_position[vtol::EAST];
+    msg->pose.position.y = _local_position[vtol::NORTH];
+    msg->pose.position.z = _local_position[vtol::UP];
+    msg->pose.orientation.x = _local_velocity[3];
+    msg->pose.orientation.y = _local_velocity[4];
+    msg->pose.orientation.z = _local_velocity[5];
+    local_pos_pub->publish(*msg);
 }
 
 void OffboardMavros::publishActuatorControls(void) {
-    mavros_msgs::msg::ActuatorControl actuator_control_msg;
-    actuator_control_msg.group_mix = 2;
-    actuator_control_msg.header.stamp = this->now();
-    actuator_control_msg.header.frame_id = "standard_vtol_0";
-    actuator_control_msg.controls[0] = 1.0f;
-    actuator_control_msg.controls[1] = 1.0f;
-    actuator_control_msg.controls[2] = 1.0f;
-    actuator_control_msg.controls[7] = 1.0f;
+    auto msg = std::make_shared<mavros_msgs::msg::ActuatorControl>();
+    msg->group_mix = 2;
+    msg->header.stamp = this->now();
+    msg->header.frame_id = "standard_vtol_0";
+    msg->controls[0] = 1.0f;
+    msg->controls[1] = 1.0f;
+    msg->controls[2] = 1.0f;
+    msg->controls[7] = 1.0f;
     RCLCPP_INFO(this->get_logger(), "publishing actuator controls");
-    actuator_control_pub->publish(actuator_control_msg);
+    actuator_control_pub->publish(*msg);
 }
 
 void OffboardMavros::publishVelocity(void) {
-    geometry_msgs::msg::Twist vel;
-    vel.linear.x = _local_velocity[0];
-    vel.linear.y = _local_velocity[1];
-    vel.linear.z = _local_velocity[2];
-    vel.angular.x = _local_velocity[3];
-    vel.angular.y = _local_velocity[4];
-    vel.angular.z = _local_velocity[5];
-    local_vel_pub->publish(vel);
+    auto msg = std::make_shared<geometry_msgs::msg::Twist>();
+    msg->linear.x = _local_velocity[0];
+    msg->linear.y = _local_velocity[1];
+    msg->linear.z = _local_velocity[2];
+    msg->angular.x = _local_velocity[3];
+    msg->angular.y = _local_velocity[4];
+    msg->angular.z = _local_velocity[5];
+    local_vel_pub->publish(*msg);
 }
 
 void OffboardMavros::publishRawLocal(void) {
     std::cout << "Publishing Raw local..." << std::endl;
-    mavros_msgs::msg::PositionTarget local_msg;
-    local_msg.header.stamp = this->now();
-    local_msg.header.frame_id = "standard_vtol_0";
-    local_msg.coordinate_frame = mavros_msgs::msg::PositionTarget::FRAME_LOCAL_NED;
-    local_msg.type_mask = mavros_msgs::msg::PositionTarget::IGNORE_PX	|
+    auto msg = std::make_shared<mavros_msgs::msg::PositionTarget>();
+
+    msg->header.stamp = this->now();
+    msg->header.frame_id = "standard_vtol_0";
+    msg->coordinate_frame = mavros_msgs::msg::PositionTarget::FRAME_LOCAL_NED;
+    msg->type_mask = mavros_msgs::msg::PositionTarget::IGNORE_PX	|
                           mavros_msgs::msg::PositionTarget::IGNORE_PY	|
                           mavros_msgs::msg::PositionTarget::IGNORE_PZ	|
                           mavros_msgs::msg::PositionTarget::IGNORE_AFX	|
                           mavros_msgs::msg::PositionTarget::IGNORE_AFY	|
                           mavros_msgs::msg::PositionTarget::IGNORE_AFZ  ;
-    // local_msg.velocity.x    = _local_velocity[0]; // East
-    // local_msg.velocity.y    = _local_velocity[1]; // North
-    // local_msg.velocity.z    = _local_velocity[2]; // Up
-    local_msg.yaw           = _local_velocity[4];
-    local_msg.yaw_rate      = _local_velocity[5];
-    local_pub->publish(local_msg);
+    // msg->velocity.x    = _local_velocity[0]; // East
+    // msg->velocity.y    = _local_velocity[1]; // North
+    // msg->velocity.z    = _local_velocity[2]; // Up
+    msg->yaw           = _local_velocity[4];
+    msg->yaw_rate      = _local_velocity[5];
+    local_pub->publish(*msg);
 }
 
 
 void OffboardMavros::publishRawAttitude(void) {
     // std::cout << "Publishing Raw Attitude ..." << std::endl;
-    mavros_msgs::msg::AttitudeTarget target_msg;
-    target_msg.header.stamp = this->now();
-    target_msg.header.frame_id = "standard_vtol_0";
-    target_msg.type_mask =  \
+    auto msg = std::make_shared<mavros_msgs::msg::AttitudeTarget>();
+    msg->header.stamp = this->now();
+    msg->header.frame_id = "standard_vtol_0";
+    msg->type_mask =  \
                             mavros_msgs::msg::AttitudeTarget::IGNORE_ROLL_RATE  |
                             mavros_msgs::msg::AttitudeTarget::IGNORE_PITCH_RATE |
                             mavros_msgs::msg::AttitudeTarget::IGNORE_YAW_RATE;
                             // mavros_msgs::msg::AttitudeTarget::IGNORE_ATTITUDE;
-                             // mavros_msgs::msg::AttitudeTarget::IGNORE_THRUST;
-
+                            // mavros_msgs::msg::AttitudeTarget::IGNORE_THRUST;
     tf2::Quaternion q;
     q.setRPY(_local_velocity[3],_local_velocity[4],_local_velocity[5]);
-    target_msg.orientation.w    =  q.w();      //
-    target_msg.orientation.x    =  q.x();      
-    // roll : 키보드 2, 3  (2 : 반시계, 3 : 시계)
-    target_msg.orientation.y    =  q.y();      //
-    target_msg.orientation.z    =  q.z();      
-    // pitch : 키보드 4, 5 (4 : 하강,   5 : 상승)
-    // target_msg.body_rate.x    =  _local_velocity[3];    
-    // target_msg.body_rate.y    =  _local_velocity[4]; 
-    // target_msg.body_rate.z    =  _local_velocity[5]; 
-
-    target_msg.thrust = _local_velocity[2];
-    raw_attitude_pub->publish(target_msg);
+    msg->orientation.w    =  q.w();      //
+    msg->orientation.x    =  q.x();      // roll : 키보드 2, 3  (2 : 반시계, 3 : 시계)
+    msg->orientation.y    =  q.y();      //
+    msg->orientation.z    =  q.z();      // pitch : 키보드 4, 5 (4 : 하강,   5 : 상승)
+    // msg->body_rate.x    =  _local_velocity[3];    
+    // msg->body_rate.y    =  _local_velocity[4]; 
+    // msg->body_rate.z    =  _local_velocity[5]; 
+    msg->thrust = _local_velocity[2];
+    raw_attitude_pub->publish(*msg);
 }
 
 
