e906cd3334968d24854e921637c10812ee703fb9
shiningsun <eojin333c@gmail.com>
2025-02-22 21:18:11 +0900

    remove last year dir

--- ws_sensor_combined_src/src/px4_ros_com/src/PRACTICE/pwm_aux_output.cpp
+++
@@ -1,9 +0,0 @@
-#include "px4_ros_com/ServoControlPublisher.hpp"
-
-int main(int argc, char * argv[])
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ServoControlPublisher>());
-    rclcpp::shutdown();
-    return 0;
-}
--- ws_sensor_combined_src/src/px4_ros_com/src/main/key_event_pub.cpp
+++
@@ -1,9 +0,0 @@
-#include "key_event/KeyPublisher.hpp"
-
-int main(int argc, char **argv) {
-	signal(SIGINT, KeyPublisher::sigintHandler);
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<KeyPublisher>());
-    rclcpp::shutdown();
-    return 0;
-}
